a945e8066d3058061d0c6671588a15513c8f37c3
Alejandro Roa <alejandro@groowly.com>
2020-03-30 09:37:55 -0400

    add function

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const Airtable = require('airtable');
 require('dotenv').config();
 
 // GLOBAL
 base = new Airtable({ apiKey: process.env.AIRTABLEKEY }).base(process.env.AIRTABLEBASE);
+citiesTable = 'Cities'
 
 /******************
  * ENDPOINTS
  ******************/
 router.get('/airtables/:country', (req, res) => {
+    base(citiesTable).select({
+        view: "Grid view"
+    }).eachPage(function page(records, fetchNextPage) {
+        records.forEach(function(record) {
+            if(record.get('country') === req.params.country){
+                console.log('Retrieved', record.get('country'));
+            }
+        });
+        fetchNextPage();
+    }, function done(err) {
+        if (err) { console.error(err); return; }
+    });
 });
 
 module.exports = router;
