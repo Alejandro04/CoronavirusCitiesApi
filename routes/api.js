de405388aaaa82cf6a3bfcf2a8c1ec4d63548c27
Alejandro Roa <alejandro@groowly.com>
2020-03-31 09:21:28 -0400

    add lastUpdate Item

--- routes/api.js
+++ routes/api.js
@@ -6,15 +6,32 @@ require('dotenv').config();
 // GLOBAL
 base = new Airtable({ apiKey: process.env.AIRTABLEKEY }).base(process.env.AIRTABLEBASE);
 citiesTable = 'Cities'
+lastUpdateTable = 'LastUpdateCities'
 
 /******************
  * ENDPOINTS
  ******************/
 router.get('/country/:country', (req, res) => {
-    getCities(req, res)
+    getLastUpdate(req, res)
 });
 
-async function getCities(req, res) {
+async function getLastUpdate(req, res){
+    let lastUpdate = ''
+    await base(lastUpdateTable).select({
+        maxRecords: 1,
+        view: "Grid view"
+    }).eachPage(function page(records, fetchNextPage) {
+        records.forEach(function(record) {
+            lastUpdate = record.get('lastUpdate')
+            getCities(req, res, lastUpdate)
+        });
+        fetchNextPage();
+    }, function done(err) {
+        if (err) { console.error(err); return; }
+    });
+}
+
+async function getCities(req, res, lastUpdate) {
     let cities = []
     await base(citiesTable).select({
         view: "Grid view",
@@ -28,15 +45,18 @@ async function getCities(req, res) {
                 })
             }
         });
-        returnObject(res, cities)
+        returnObject(res, cities, lastUpdate)
         fetchNextPage();
     }, function done(err) {
         if (err) { console.error(err); return; }
     });
 }
 
-function returnObject(res, cities) {
-    res.json(cities)
+function returnObject(res, cities, lastUpdate) {
+    res.json({
+        cities,
+        lastUpdate
+    })
 }
 
 module.exports = router;
