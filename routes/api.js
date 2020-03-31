ce24530cb0a8bec457c3abd4d56f7050477f4bc8
Alejandro Roa <alejandro@groowly.com>
2020-03-30 23:33:06 -0400

    wip

--- routes/api.js
+++ routes/api.js
@@ -1,28 +1,42 @@
 const express = require('express');
-const router = express.Router();
 const Airtable = require('airtable');
+const router = express.Router();
 require('dotenv').config();
 
 // GLOBAL
 base = new Airtable({ apiKey: process.env.AIRTABLEKEY }).base(process.env.AIRTABLEBASE);
 citiesTable = 'Cities'
+cities = []
 
 /******************
  * ENDPOINTS
  ******************/
-router.get('/airtables/:country', (req, res) => {
-    base(citiesTable).select({
+router.get('/coronavirusapi/:country', (req, res) => {
+    getCities(req, res)
+});
+
+async function getCities(req, res) {
+    await base(citiesTable).select({
         view: "Grid view"
     }).eachPage(function page(records, fetchNextPage) {
-        records.forEach(function(record) {
-            if(record.get('country') === req.params.country){
-                console.log('Retrieved', record.get('country'));
+        records.forEach(function (record) {
+            if (record.get('country') === req.params.country) {
+                cities.push({
+                    state: record.get('state'),
+                    cases: record.get('cases'),
+                    deaths: record.get('deaths')
+                })
             }
         });
+        returnObject(res)
         fetchNextPage();
     }, function done(err) {
         if (err) { console.error(err); return; }
     });
-});
+}
+
+function returnObject(res) {
+    res.json(cities)
+}
 
 module.exports = router;
